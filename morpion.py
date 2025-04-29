b5d80ee014c9c910c7508f6b981f8ba08006e25b
CVincent27 <cardon.vincent@hotmail.fr>
2025-04-29 09:39:46 +0200

    add jouer_coup

--- morpion.py
+++ morpion.py
@@ -1,8 +1,26 @@
 grille_morpion = {"A":["_","_","_"],"B":["_","_","_"],"C":["_","_","_"]}
 
-def afficher_grille():
-    for valeur in grille_morpion.values():
-        print(valeur)
+while True:
 
-def afficher_test():
-    print('hello')
+    def afficher_grille():
+        print('   1     2     3')
+        for valeur, item in grille_morpion.items():
+            print(valeur, item)
+        
+        print(grille_morpion['A'][0])
+
+
+    def jouer_coup():
+        print('test')
+        joueur = 'X'
+        colonne = int(input('choisir une colonne: 1, 2, 3 ?'))
+        ligne = input('choisir ligne ? A, B, C')
+        grille_morpion[ligne][colonne -1] = joueur
+
+
+
+
+    jouer_coup()
+    afficher_grille()
+    
+
